4d98fbccc8788eb4861928340f65d85c786011c6
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-28 22:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.87%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -13,15 +13,32 @@ public:
 
         // }
         // return res;
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] != i){
+        //         return i;
+
+        //     } 
+        // }
+        // return n;
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++){
-            if(nums[i] != i){
-                return i;
-
-            } 
+        int l   = 0;
+        int r   = n-1;
+        int result = n;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] > mid) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
         }
-        return n;
+        
+        return result;
     }
         
     
