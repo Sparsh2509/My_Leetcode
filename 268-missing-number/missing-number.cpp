9e182ffc9f5ce8636bbe6cdd4ff4b044b3d47fd3
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-28 22:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (37.87%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // int n =nums.size();
-        // int res = n;
+        int n =nums.size();
+        int res = n;
 
-        // // int sum = (n*(n+1))/2;
+        // int sum = (n*(n+1))/2;
 
-        // for(int i = 0;i<n;i++){
-        //     // sum = sum - nums[i];
-        //     res = res ^ i;
-        //     res = res ^ nums[i];
+        for(int i = 0;i<n;i++){
+            // sum = sum - nums[i];
+            res = res ^ i;
+            res = res ^ nums[i];
 
-        // }
-        // return res;
+        }
+        return res;
+
+        // SORT + TRAVERSE APPROACH
+
         // int n = nums.size();
         // sort(nums.begin(), nums.end());
         // for(int i = 0; i < n; i++){
@@ -22,23 +25,26 @@ public:
         //     } 
         // }
         // return n;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int l   = 0;
-        int r   = n-1;
-        int result = n;
+
+        //   BINARY SEARCH APPROACH
+
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // int l   = 0;
+        // int r   = n-1;
+        // int result = n;
         
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            if(nums[mid] > mid) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
-            }
-        }
+        // while(l <= r) {
+        //     int mid = l + (r-l)/2;
+        //     if(nums[mid] > mid) {
+        //         result = mid;
+        //         r = mid-1;
+        //     } else {
+        //         l = mid+1;
+        //     }
+        // }
         
-        return result;
+        // return result;
     }
         
     
