08a0393984e3a960dd8d2c09380fc739f2f53897
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-13 22:00:31 +0530

    Time: 1 ms (58.82%) | Memory: 8.6 MB (68.26%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long double temp,a=0,rem,pal=0;
+        temp=x;
+         while(x>0){
+            rem=x%10;
+            pal=pal*10+rem;
+            x=x/10;
+        }
+        if (temp==pal){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
