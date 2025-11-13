f237560d548082859eb6ce7c9f4707ea8f55d555
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-13 21:51:53 +0530

    Time: 7 ms (43.12%) | Memory: 15.9 MB (26.09%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ones = 0, res = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '1')
-                ones++;
-            else if ((i > 0) && s[i - 1] == '1')
-                res += ones;
+        int n          = s.length();
+        int result     = 0;
+        int i          = 0;
+        int count1seen = 0;
+
+        while(i < n) {
+            if(s[i] == '0') {
+                result += count1seen;
+                while(i < n && s[i] == '0') //moved till first '0' occurence
+                    i++;
+            } else {
+                count1seen++;
+                i++;
+            }
         }
 
-        return res;
+        return result;
         
     }
 };
