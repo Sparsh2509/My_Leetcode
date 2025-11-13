de9aa94e720bf53c1e6f3b5b4489fb763109846a
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-13 21:52:49 +0530

    Time: 3 ms (83.33%) | Memory: 15.8 MB (75.00%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int n          = s.length();
-        int result     = 0;
-        int i          = 0;
-        int count1seen = 0;
+        // int n          = s.length();
+        // int result     = 0;
+        // int i          = 0;
+        // int count1seen = 0;
 
-        while(i < n) {
-            if(s[i] == '0') {
-                result += count1seen;
-                while(i < n && s[i] == '0') //moved till first '0' occurence
-                    i++;
-            } else {
-                count1seen++;
-                i++;
-            }
+        // while(i < n) {
+        //     if(s[i] == '0') {
+        //         result += count1seen;
+        //         while(i < n && s[i] == '0') //moved till first '0' occurence
+        //             i++;
+        //     } else {
+        //         count1seen++;
+        //         i++;
+        //     }
+        // }
+
+        // return result;
+        int ones = 0, res = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '1')
+                ones++;
+            else if ((i > 0) && s[i - 1] == '1')
+                res += ones;
         }
+        return res;
 
-        return result;
+
         
     }
 };
