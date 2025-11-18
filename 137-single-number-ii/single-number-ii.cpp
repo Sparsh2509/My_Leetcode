a06bbad8e6f8891d3ae26d9484dc319a3c5b0bc0
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-18 18:21:18 +0530

    Time: 3 ms (32.72%) | Memory: 14 MB (21.63%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto mpp : mp){
+            if (mpp.second==1){
+                return mpp.first;
+            }
+        }
+       return 0; 
+        
+        
+    }
+};
