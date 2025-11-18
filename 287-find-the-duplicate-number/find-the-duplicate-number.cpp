d80e94820573722bb7961119199768e4e374c516
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-18 18:27:16 +0530

    Time: 111 ms (9.86%) | Memory: 104.8 MB (5.33%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            
+        }
+
+        for(auto mpp:mp){
+            if(mpp.second>1){
+                return mpp.first;
+            }
+        }
+        return 0;
+        
+    }
+};
