2e82c3e97ad9ff4d1948c2c9f8df208720fe7c62
Sparsh2509 <sparshgupta250904@gmail.com>
2025-11-27 08:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (81.30%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        size_t pos = haystack.find(needle);
-        return pos == string::npos ? -1 : (int)pos;       
+        // size_t pos = haystack.find(needle);
+        // return pos == string::npos ? -1 : (int)pos;       
         // STL APPROACH
 
 
         // Brute Force 
 
-        // int n = haystack.size();    
-        // int m = needle.size();
+        int n = haystack.size();    
+        int m = needle.size();
         
-        // if (m == 0) return 0;  // if needle is empty return 0
+        if (m == 0) return 0;  // if needle is empty return 0
         
-        // for (int i = 0; i <= n - m; i++) {   // loop through haystack
-        //     int j = 0;
-        //     while (j < m && haystack[i + j] == needle[j]) {  // match characters
-        //         j++;
-        //     }
-        //     if (j == m) return i; // whole needle matched
-        // }
-        // return -1; // not found
+        for (int i = 0; i <= n - m; i++) {   // loop through haystack
+            int j = 0;
+            while (j < m && haystack[i + j] == needle[j]) {  // match characters
+                j++;
+            }
+            if (j == m) return i; // whole needle matched
+        }
+        return -1; // not found
         
     }
 };
