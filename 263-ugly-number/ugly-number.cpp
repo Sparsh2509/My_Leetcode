8d58ca1cae7d22d8a35007118978a9ea939b88f3
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-02-03 15:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (42.40%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        while (n > 0) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 5 == 0) {
-                n /= 5;
-            } else {
-                if (n == 1) {
+        while(n>0){
+            if(n%2==0){
+                n=n/2;
+            }else if(n%3==0){
+                n=n/3;
+            }else if(n%5==0){
+                n=n/5;
+            }else{
+                if(n==1){
                     return true;
                 }
                 break;
             }
+
         }
         return false;
+        
     }
 };
