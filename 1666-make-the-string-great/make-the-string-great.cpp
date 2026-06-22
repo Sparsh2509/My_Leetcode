e731f0c3c057979658cf96ff177b60c7a5b6fd13
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-06-22 18:35:02 +0530

    Time: 3 ms (13.52%) | Memory: 8.4 MB (67.36%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string st;
+
+        for(char ch : s) {
+            if(!st.empty() && abs(st.back() - ch) == 32) {
+                st.pop_back();
+            }
+            else {
+                st.push_back(ch);
+            }
+        }
+
+        return st;
+        
+    }
+};
