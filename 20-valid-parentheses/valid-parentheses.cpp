628c0e645a390553bf1cbdb900c1f7ed2cbefea3
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-06-22 14:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (10.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>a;
+        stack<char>st;
         if(s.length() % 2 !=0){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            if(a.empty()){
-               a.push(s[i]); 
+
+        for(char  &ch : s ){
+            if(ch == '('){
+                st.push(')');
             }
-            else if ((a.top() == '(' && s[i] == ')')
-                 || (a.top() == '{' && s[i] == '}')
-                 || (a.top() == '[' && s[i] == ']'))
-            {
-                a.pop();
+            else if(ch == '{'){
+                st.push('}');
+            }
+            else if(ch == '['){
+                st.push(']');
+            }
+            else if(st.empty() || st.top()!= ch){
+                return false;
             }
             else{
-                a.push(s[i]);
-            }    
+                st.pop();
+            }
         }
-        if(a.empty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+        return st.empty();
+        // for(int i=0;i<s.length();i++){
+        //     if(a.empty()){
+        //        a.push(s[i]); 
+        //     }
+        //     else if ((a.top() == '(' && s[i] == ')')
+        //          || (a.top() == '{' && s[i] == '}')
+        //          || (a.top() == '[' && s[i] == ']'))
+        //     {
+        //         a.pop();
+        //     }
+        //     else{
+        //         a.push(s[i]);
+        //     }    
+        // }
+        // if(a.empty()){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
         
     }
 };
