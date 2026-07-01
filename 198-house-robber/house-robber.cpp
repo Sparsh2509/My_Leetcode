acca615f0098f8b4e3131f1f7b48517df6dce767
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-07-01 23:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.80%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,36 +1,21 @@
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         if (n == 0) return 0;
-//         if (n == 1) return nums[0];
-
-//         vector<int> dp(n);
-//         dp[0] = nums[0];
-//         dp[1] = max(nums[0], nums[1]);
-
-//         for (int i = 2; i < n; i++) {
-//             dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
-//         }
-
-//         return dp[n - 1];
-        
-//     }
-    
-// };
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev1 = 0;
-        int prev2 = 0;
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-        for(int num : nums){
-            int temp = max(prev1, prev2 + num);
-            prev2 = prev1;
-            prev1 = temp;
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
         }
 
-        return prev1;
+        return dp[n - 1];
+        
     }
+    
 };
+
