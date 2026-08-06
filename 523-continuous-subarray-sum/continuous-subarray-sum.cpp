dd1d6c57c2987de249fd837324781ad75124d3cf
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-08-07 00:59:37 +0530

    Time: 80 ms (39.99%) | Memory: 146.2 MB (86.92%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> firstIndex;
+        firstIndex[0] = -1;
+
+        int prefix = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix += nums[i];
+            int rem = prefix % k;
+
+            if (firstIndex.count(rem)) {
+                if (i - firstIndex[rem] >= 2)
+                    return true;
+            } else {
+                // Store only the first occurrence
+                firstIndex[rem] = i;
+            }
+        }
+
+        return false;
+        
+    }
+};
