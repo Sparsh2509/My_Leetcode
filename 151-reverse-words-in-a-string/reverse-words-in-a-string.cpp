6f2455837fbf1672530586d451cced3cfd3f86ea
Sparsh Gupta <sparshgupta250904@gmail.com>
2026-08-08 10:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (95.17%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(),s.end());
-        int i = 0;
-        int l = 0 , r =0 ;
-        int n =s.length();
-        while(i<n){
-            while(i<n && s[i] != ' '){
-                s[r++]=s[i++];
+        reverse(s.begin(), s.end());
 
+        int i = 0;
+        int l = 0, r = 0;
+        int n = s.length();
+
+        while (i < n) {
+
+            // Skip extra spaces
+            while (i < n && s[i] == ' ')
+                i++;
+
+            // Copy current word
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
             }
-            if (l < r){
-                reverse(s.begin()+l,s.begin()+r);
-                s[r]= ' ';
-                r++;
+
+            // Reverse current word
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+
+                // Put one space after word
+                s[r++] = ' ';
+
                 l = r;
             }
-            i++;
-
         }
-        s= s.substr(0 , r-1);
+
+        // Remove last extra space
+        if (r > 0)
+            r--;
+
+        s.resize(r);
+
         return s;
-        
     }
 };
